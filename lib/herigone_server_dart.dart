d0dbd2929035f0c1e002d4f8e900681f8d925e2d
Ville Peurala <ville.peurala@gmail.com>
2019-06-29 17:28:11 +0300

    Initial commit

---
+++ lib/herigone_server_dart.dart
@@ -0,0 +1,12 @@
+/// herigone_server_dart
+///
+/// A Aqueduct web server.
+library herigone_server_dart;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+export 'package:aqueduct/managed_auth.dart';
+
+export 'channel.dart';
